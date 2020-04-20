9473d1d2dfc908189c67c0eaea152cf035c3d20e
nikprotsman <nikprotsman@gmail.com>
2020-04-20 11:46:12 -0600

    Add project files.

---
+++ BlazorUIEvents/Data/SingleService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUIEvents.Data
+{
+	public class SingleService
+	{
+		public delegate void MyDel(object sender, EventArgs e);
+
+		public event MyDel MyEvent;
+
+		public int CountValue;
+
+		public void OnCountValueIncreased(object sender, EventArgs e)
+		{
+			CountValue++;
+			MyEvent?.Invoke(sender, e);
+		}
+
+	}
+
+	public class StringEventArgs : EventArgs
+	{
+		public string Value { get; set; }
+	}
+}
---
+++ BlazorUIEvents/Pages/Index.razor.cs
@@ -0,0 +1,44 @@
+﻿using BlazorUIEvents.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUIEvents.Pages
+{
+	public class IndexBase : OwningComponentBase
+	{
+
+		public string status { get; set; }
+
+		public string WhereAmI { get; set; }
+
+		[Inject]
+		protected SingleService Service { get; set; }
+
+
+		private async void OnStatusChangedMethod(object sender, EventArgs e)
+		{
+
+			if (e.GetType() == typeof(StringEventArgs))
+			{
+				status = (e as StringEventArgs).Value;
+			}
+
+			await InvokeAsync(() => StateHasChanged());
+		}
+
+		protected override void OnInitialized()
+		{
+			Service.MyEvent -= OnStatusChangedMethod;
+			Service.MyEvent += OnStatusChangedMethod;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			Service.MyEvent -= OnStatusChangedMethod;
+		}
+
+	}
+}
